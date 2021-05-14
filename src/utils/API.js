b7e43c4abb36d623293003688f24a51870e52ec2
Austn Hughes <austen.hughes.91@gmail.com>
2021-05-14 13:34:12 -0600

    gh pages

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,9 @@
 import axios from "axios";
   
 const EmployeeSet = "https://randomuser.me/api/?results=20";
-  // "https://randomuser.me/api/?inc=picture,name,phone,email,dob&results=15";
   
   export default {
-    getEmployeeSet: function(query) {
+    getEmployees: function(query) {
     return axios.get(EmployeeSet);
     }
   };
